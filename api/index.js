1b13ed403e3450fb98937db0f34cb155b7930364
Sattr <airryys@gmail.com>
2026-04-06 13:40:22 +0700

    fixing cors issue

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-// Middleware untuk format JSON
+
+app.use(cors());
 app.use(express.json());
 
 // Data Menu Restoran Lengkap
